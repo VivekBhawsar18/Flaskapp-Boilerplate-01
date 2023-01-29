fb673f0af806dba89ee601d56fcf0ff4bf602898
VivekBhawsar18 <vivekcoder18@gmail.com>
2023-01-29 14:26:00 +0530

    up

--- Flaskapp/__init__.py
+++ Flaskapp/__init__.py
@@ -24,3 +24,22 @@
 
 
 #     return app
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+        # return str(os.environ.get('SECRET_KEY'))
+        # return str(os.environ.get('SQLALCHEMY_DATABASE_URI'))
+        return '<h1>Home page of Flask Application Factory Pattern</h1>'
+
+@app.route('/test/')
+def test_page():
+        # return str(os.environ.get('SECRET_KEY'))
+        # return str(os.environ.get('SQLALCHEMY_DATABASE_URI'))
+        return '<h1>Testing the Flask Application Factory Pattern</h1>'
+
+if __name__ == '__main__':
+    app.run()
--- app.py
+++ app.py
@@ -4,21 +4,28 @@
 # if __name__ == '__main__':
 #     create_app().run()
 
-from flask import Flask
+# -------------------------------------------------------------------------
 
-app = Flask(__name__)
+# from flask import Flask
 
-@app.route('/')
-def home():
-        # return str(os.environ.get('SECRET_KEY'))
-        # return str(os.environ.get('SQLALCHEMY_DATABASE_URI'))
-        return '<h1>Home page of Flask Application Factory Pattern</h1>'
+# app = Flask(__name__)
 
-@app.route('/test/')
-def test_page():
-        # return str(os.environ.get('SECRET_KEY'))
-        # return str(os.environ.get('SQLALCHEMY_DATABASE_URI'))
-        return '<h1>Testing the Flask Application Factory Pattern</h1>'
+# @app.route('/')
+# def home():
+#         # return str(os.environ.get('SECRET_KEY'))
+#         # return str(os.environ.get('SQLALCHEMY_DATABASE_URI'))
+#         return '<h1>Home page of Flask Application Factory Pattern</h1>'
+
+# @app.route('/test/')
+# def test_page():
+#         # return str(os.environ.get('SECRET_KEY'))
+#         # return str(os.environ.get('SQLALCHEMY_DATABASE_URI'))
+#         return '<h1>Testing the Flask Application Factory Pattern</h1>'
+
+# if __name__ == '__main__':
+#     app.run()
+
+from Flaskapp import app
 
 if __name__ == '__main__':
-    app.run()
+    app.run() 
