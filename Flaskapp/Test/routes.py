96b7b7cd854302b657101aff30201efe3cb10e35
VivekBhawsar18 <vivekcoder18@gmail.com>
2023-01-29 16:14:38 +0530

    up

--- Flaskapp/Test/routes.py
+++ Flaskapp/Test/routes.py
@@ -1,7 +1,13 @@
 from flask import render_template , Blueprint
+from config import Config
 
 bp = Blueprint('Test' , __name__)
 
 @bp.route('/')
 def index_test():
     return render_template('index.html')
+
+@bp.route('/db')
+def test_db():
+    return( f"{Config.SQLALCHEMY_DATABASE_URI}{Config.SQLALCHEMY_TRACK_MODIFICATIONS}\t{Config.SECRET_KEY}")
+
