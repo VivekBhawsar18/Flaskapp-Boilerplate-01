2dff51c397b016c11601b78e2309e88306a87be0
VivekBhawsar18 <vivekcoder18@gmail.com>
2023-01-29 16:06:53 +0530

    up

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/cndb'
-    SECRET_KEY= 'B@Vv$st^<b^NJt/'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://vivek01:Vikas#9789@mysql-server-demoflask.mysql.database.azure.com/cndb'
+    # SECRET_KEY= 'B@Vv$st^<b^NJt/'
+    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://vivek01:Vikas#9789@mysql-server-demoflask.mysql.database.azure.com/cndb'
     print(SQLALCHEMY_DATABASE_URI)
     print(SECRET_KEY)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
