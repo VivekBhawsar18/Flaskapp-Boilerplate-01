20e63c65a76c8515ba71389d60dcb87638058ef4
VivekBhawsar18 <vivekcoder18@gmail.com>
2023-01-29 16:46:28 +0530

    up

--- Flaskapp/Test/routes.py
+++ Flaskapp/Test/routes.py
@@ -7,6 +7,10 @@ bp = Blueprint('Test' , __name__)
 def index_test():
     return render_template('index.html')
 
+@bp.route('/home')
+def index_home():
+    return "<h1>Home page of blueprint</h1>"
+
 @bp.route('/db')
 def test_db():
     return( f"{Config.SQLALCHEMY_DATABASE_URI}{Config.SQLALCHEMY_TRACK_MODIFICATIONS}\t{Config.SECRET_KEY}")
