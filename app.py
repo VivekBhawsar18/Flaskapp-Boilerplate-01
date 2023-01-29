08ee450953bbdd1e6ec05f1abcbc8a7a42a93b2d
VivekBhawsar18 <vivekcoder18@gmail.com>
2023-01-29 14:20:36 +0530

    up

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
-from Flaskapp import create_app
+# from Flaskapp import create_app
 
+
+# if __name__ == '__main__':
+#     create_app().run()
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+        # return str(os.environ.get('SECRET_KEY'))
+        # return str(os.environ.get('SQLALCHEMY_DATABASE_URI'))
+        return '<h1>Home page of Flask Application Factory Pattern</h1>'
+
+@app.route('/test/')
+def test_page():
+        # return str(os.environ.get('SECRET_KEY'))
+        # return str(os.environ.get('SQLALCHEMY_DATABASE_URI'))
+        return '<h1>Testing the Flask Application Factory Pattern</h1>'
 
 if __name__ == '__main__':
-    create_app().run()
+    app.run()
